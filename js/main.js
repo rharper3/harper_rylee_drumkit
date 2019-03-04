b5c892deb86170b86883d900dcb61ea3b25fe668
Harper <r_harper3@tss-hr420-dm08.fc.ca>
2019-03-04 16:08:57 -0500

    did css stuff

--- js/main.js
+++ js/main.js
@@ -16,7 +16,28 @@
 		//rewind audio on every click and make it play
 		audio.currentTime = 0;
 		audio.play();
+
+		// grab the div and animate it
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+		key.classList.add('playing');
 	}
+
+	function removePlayingClass(event) {
+		// debugger;
+		// 
+		if (event.propertyName !== "transform") {
+			return;
+		} else {
+			//remove the playing class here from the active div
+			console.log('transition is done!');
+			event.target.classList.remove('playing');
+		}
+		
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
 
 	window.addEventListener("keydown", playDrumKitSound);
 })();
